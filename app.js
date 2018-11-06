6b6dba0a3d0328c7ce22769e15329809c7743ab8
Alexander Oswari <alexoswari@gmail.com>
2018-11-06 00:47:07 -0500

    First commit

---
+++ app.js
@@ -0,0 +1,17 @@
+//Core Modules
+const path = require('path');
+//Third Party Imports
+const express = require('express');
+//Custom Imports
+
+const app = express();
+
+app.use('/users',(req,res,next) => {
+    
+});
+
+app.use('/',(req,res,next)=> {
+    
+});
+
+app.listen(3000);
